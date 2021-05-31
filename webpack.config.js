d9e18774a3d56223abafd6ae910b2bc1933cbceb
Xavi Ferro <xferro@gmail.com>
2021-05-31 12:15:58 +1000

    Fixing more bundling problems

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'index.bundle.js',
+    filename: 'index.js',
   },
   module: {
     rules: [ {
